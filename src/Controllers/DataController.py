063bdd2f7e5f3a9090b4192f9b050b42381530ef
Dina Mohamed <dinam5862@gmail.com>
2025-08-11 05:28:35 +0300

    Upload Fil enpoint , return status code

--- src/Controllers/DataController.py
+++ src/Controllers/DataController.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI,APIRouter,Depends,UploadFile
 from .BaseController import BaseController
-
+from Models import Response_Signal
 
 class DataController(BaseController):
     def __init__(self):
@@ -9,7 +9,7 @@ class DataController(BaseController):
     
     def Validate_Uploaded_File(self,file:UploadFile):
         if file.content_type  not in self.app_settings.FILE_ALLOWED_TYPES:
-           return False
+           return False,Response_Signal.FILE_TYPE_NOTSUPPORTED.value
         if file.size>self.app_settings.FILE_MAX_SIZE*self.size_scale:
-            return False
-        return True
+            return False , Response_Signal.FILE_MAX_SIZE_EXCEEDED.value
+        return True , Response_Signal.FILE_VALIDATE_SUCCESS.value
--- src/Routes/data.py
+++ src/Routes/data.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI,APIRouter,Depends,UploadFile
+from fastapi import FastAPI,APIRouter,Depends,UploadFile,status
 import os
 from Helpers.config import getSettings
 from Controllers import DataController
+from fastapi.responses import  JSONResponse
 
 data_Router=APIRouter(
     prefix="/api/v1/data",
@@ -10,5 +11,16 @@ data_Router=APIRouter(
 
 @data_Router.post("/upload/{project_id}")
 async def uploaFile(project_id:str,file:UploadFile,AppSetting=Depends(getSettings)):
-    IsValid=DataController().Validate_Uploaded_File(file=file)
-    return IsValid
+    IsValid,resultSignal=DataController().Validate_Uploaded_File(file=file)
+    if  not IsValid :
+         return JSONResponse(
+             status_code=status.HTTP_400_BAD_REQUEST,
+             content={
+                 "Signal":resultSignal
+             }
+         )
+        
+    
+        
+    
+   
